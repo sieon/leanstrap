7ee3ed1a5ed5d969eecfa45ed55f51412570820d
mxy <sieon@outlook.com>
2017-03-29 15:24:17 +0800

    删掉多余文件，调整js、css位置

--- functions.php
+++ functions.php
@@ -230,11 +230,11 @@ function lean_scripts() {
 
 	//wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
 
-	wp_enqueue_script( 'start-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'start-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'holder', '//cdn.bootcss.com/holder/2.9.4/holder.min.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'start-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'start-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
