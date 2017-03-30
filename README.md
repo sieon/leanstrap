56ba20276252a67ef76a11a10871f51e7f27325c
mxy <sieon@outlook.com>
2017-03-30 16:12:02 +0800

    修改README以及文章列表样式

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lean-wpbs4
 wordpress bootstrap主题框架
+## 主要功能
+- 点击无限加载
+- font-awesome 图标库
+- bootstrap4
+- 小工具 Widget
+- 两栏
+- Customizer library库
+- masonry.pkgd.min.js
+- Tether
+- holder.js
+- Stepu
+    - add theme support
+    - loop
+    - scripts
--- template-parts/content-bloglist.php
+++ template-parts/content-bloglist.php
@@ -8,36 +8,38 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('entry mb-4 row'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('entry mb-4 pb-3'); ?>>
+	<div class="row">
+		<?php if(has_post_thumbnail()) : ?>
+		<div class="col-lg-3">
+			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+				<?php
+					// Post thumbnail.
+					lean_post_thumbnail();
+				?>
+			</a>
+		</div>
+		<div class="col-lg-9">
+		<?php else : ?>
+		<div class="col-lg-12">
+		<?php endif; ?>
 
-	<?php if(has_post_thumbnail()) : ?>
-	<div class="col-lg-3">
-		<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
-			<?php
-				// Post thumbnail.
-				lean_post_thumbnail();
-			?>
-		</a>
-	</div>
-	<div class="col-lg-9">
-	<?php else : ?>
-	<div class="col-lg-12">
-	<?php endif; ?>
+			<header class="entry-header mb-3">
+				<?php the_title( sprintf( '<h3 class="mb-3"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+			</header>
 
-		<header class="entry-header mb-3">
-			<?php the_title( sprintf( '<h3 class="mt-0 mb-2"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-		</header>
+			<div class="entry-excerpt">
+				<?php the_excerpt(); ?>
+			</div>
 
-		<div class="entry-excerpt">
-			<?php the_excerpt(); ?>
+			<footer class="entry-footer pb-2">
+					<small>
+						<i class="fa fa-user" aria-hidden="true"></i> <?php echo get_the_author(); ?>
+						<?php echo the_time(); ?>
+						<?php lean_entry_footer(); ?>
+					</small>
+			</footer><!-- .entry-footer -->
 		</div>
 
-		<footer class="entry-footer pb-2">
-				<small>
-					<i class="fa fa-user" aria-hidden="true"></i> <?php echo get_the_author(); ?>
-					<?php echo the_time(); ?>
-					<?php lean_entry_footer(); ?>
-				</small>
-		</footer><!-- .entry-footer -->
-	</div>
+	</div><!--./row -->
 </article><!-- #post-## -->
