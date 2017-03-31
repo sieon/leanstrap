451ecc8b21a17e98ba9c8b90fc29e5b953579177
mxy <sieon@outlook.com>
2017-03-31 15:20:21 +0800

    添加carousel

--- front-page.php
+++ front-page.php
@@ -24,70 +24,29 @@ get_header('2'); ?>
         <li data-target="#myCarousel" data-slide-to="2" class=""></li>
       </ol>
       <div class="carousel-inner" role="listbox">
-        <div class="carousel-item">
-          <img class="first-slide" src="http://demo.qingzhuti.com/wp-content/uploads/2017/03/ballet-1376250_640.jpg" alt="First slide">
-          <div class="container">
-            <div class="carousel-caption d-none d-md-block text-left">
-              <h1>Example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
+        <?php $slider = get_posts(array('post_type' => 'carousel', 'posts_per_page' => 3)); ?>
+        <?php $count = 0; ?>
+        <?php foreach($slider as $slide): ?>
+        <div class="carousel-item <?php echo ($count == 0) ? 'active' : ''; ?>">
+          <img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($slide->ID)) ?>" class="d-block img-fluid"/>
+          <div class="carousel-caption d-none d-md-block">
+            <h1>Another example headline.</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
           </div>
         </div>
-        <div class="carousel-item active">
-          <img class="second-slide" src="http://demo.qingzhuti.com/wp-content/uploads/2017/03/133009858417.jpg" alt="Second slide">
-          <div class="container">
-            <div class="carousel-caption d-none d-md-block">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img class="third-slide" src="http://demo.qingzhuti.com/wp-content/uploads/2017/03/coast-192979-e1490516430557.jpg" alt="Third slide">
-          <div class="container">
-            <div class="carousel-caption d-none d-md-block text-right">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
+        <?php $count++; ?>
+        <?php endforeach; ?>
       </div>
       <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span class="sr-only">上一个</span>
       </a>
       <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span class="sr-only">下一个</span>
       </a>
     </div>
-  </div>
-  <div class="col-lg-12">
-    <?php
-    $args = array(
-      'post_type' => 'post' ,
-      'tag' => 'slide',
-      'posts_per_page' => '1'
-      //'post__in' => get_option( 'sticky_posts' )
-    );
-    $myPosts = new WP_Query($args);
-    while ( $myPosts->have_posts() ) : $myPosts->the_post();
-    ?>
-    <section class="jumbotron text-center">
-      <?php the_title( '<h1 class="jumbotron-heading">', '</h1>' ); ?>
-      <p class="lead text-muted"><?php the_excerpt();?></p>
-      <p>
-        <a href="#" class="btn btn-primary">下载</a>
-        <a href="#" class="btn btn-secondary">了解更多</a>
-      </p>
-    </section>ß
-    <?php endwhile;
-    // Reset Post Data
-    wp_reset_postdata();
-    ?>
   </div>
 
   <div class="col-lg-8">
--- functions.php
+++ functions.php
@@ -390,3 +390,106 @@ require get_template_directory() . '/inc/mods.php';
  *   下一段代码
  *
  */
+ if ( ! function_exists('lean_carousels_post_type') ) {
+
+ // Register Custom Post Type
+ function lean_carousels_post_type() {
+
+ 	$labels = array(
+ 		'name'                  => _x( 'Carousels', 'Post Type General Name', 'lean' ),
+ 		'singular_name'         => _x( 'Carousel', 'Post Type Singular Name', 'lean' ),
+ 		'menu_name'             => __( 'Carousels', 'lean' ),
+ 		'name_admin_bar'        => __( 'Carousel', 'lean' ),
+ 		'archives'              => __( 'Carousel Archives', 'lean' ),
+ 		'attributes'            => __( 'Carousel Attributes', 'lean' ),
+ 		'parent_item_colon'     => __( 'Parent Carousel:', 'lean' ),
+ 		'all_items'             => __( 'All Carousels', 'lean' ),
+ 		'add_new_item'          => __( 'Add New Carousel', 'lean' ),
+ 		'add_new'               => __( 'Add Carousel', 'lean' ),
+ 		'new_item'              => __( 'New Carousel', 'lean' ),
+ 		'edit_item'             => __( 'Edit Carousel', 'lean' ),
+ 		'update_item'           => __( 'Update Carousel', 'lean' ),
+ 		'view_item'             => __( 'View Carousel', 'lean' ),
+ 		'view_items'            => __( 'View Carousels', 'lean' ),
+ 		'search_items'          => __( 'Search Carousel', 'lean' ),
+ 		'not_found'             => __( 'Not found', 'lean' ),
+ 		'not_found_in_trash'    => __( 'Not found in Trash', 'lean' ),
+ 		'featured_image'        => __( 'Featured Image', 'lean' ),
+ 		'set_featured_image'    => __( 'Set featured image', 'lean' ),
+ 		'remove_featured_image' => __( 'Remove featured image', 'lean' ),
+ 		'use_featured_image'    => __( 'Use as featured image', 'lean' ),
+ 		'insert_into_item'      => __( 'Insert into item', 'lean' ),
+ 		'uploaded_to_this_item' => __( 'Uploaded to this item', 'lean' ),
+ 		'items_list'            => __( 'Items list', 'lean' ),
+ 		'items_list_navigation' => __( 'Items list navigation', 'lean' ),
+ 		'filter_items_list'     => __( 'Filter items list', 'lean' ),
+ 	);
+ 	$args = array(
+ 		'label'                 => __( 'Carousel', 'lean' ),
+ 		'description'           => __( 'Carousel Description', 'lean' ),
+ 		'labels'                => $labels,
+ 		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'page-attributes', ),
+ 		'taxonomies'            => array( 'carousel-category' ),
+ 		'hierarchical'          => true,
+ 		'public'                => true,
+ 		'show_ui'               => true,
+ 		'show_in_menu'          => true,
+ 		'menu_position'         => 5,
+ 		'show_in_admin_bar'     => true,
+ 		'show_in_nav_menus'     => true,
+ 		'can_export'            => true,
+ 		'has_archive'           => false,
+ 		'exclude_from_search'   => false,
+ 		'publicly_queryable'    => true,
+ 		'capability_type'       => 'page',
+ 	);
+ 	register_post_type( 'carousel', $args );
+
+ }
+ add_action( 'init', 'lean_carousels_post_type', 0 );
+
+ }
+
+ if ( ! function_exists( 'lean_carousel_category' ) ) {
+
+// Register Custom Taxonomy
+function lean_carousel_category() {
+
+	$labels = array(
+		'name'                       => _x( 'carousel-category', 'Taxonomy General Name', 'lean' ),
+		'singular_name'              => _x( 'carousel-category', 'Taxonomy Singular Name', 'lean' ),
+		'menu_name'                  => __( 'carousel-category', 'lean' ),
+		'all_items'                  => __( 'All categories', 'lean' ),
+		'parent_item'                => __( 'Parent Category', 'lean' ),
+		'parent_item_colon'          => __( 'Parent Category:', 'lean' ),
+		'new_item_name'              => __( 'New Category Name', 'lean' ),
+		'add_new_item'               => __( 'Add New Category', 'lean' ),
+		'edit_item'                  => __( 'Edit Category', 'lean' ),
+		'update_item'                => __( 'Update Category', 'lean' ),
+		'view_item'                  => __( 'View Category', 'lean' ),
+		'separate_items_with_commas' => __( 'Separate categories with commas', 'lean' ),
+		'add_or_remove_items'        => __( 'Add or remove Categories', 'lean' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'lean' ),
+		'popular_items'              => __( 'Popular Categories', 'lean' ),
+		'search_items'               => __( 'Search Items', 'lean' ),
+		'not_found'                  => __( 'Not Found', 'lean' ),
+		'no_terms'                   => __( 'No items', 'lean' ),
+		'items_list'                 => __( 'Items list', 'lean' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'lean' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => false,
+		'rewrite'                    => false,
+	);
+	register_taxonomy( 'carousel-category', array( 'carousel' ), $args );
+
+}
+add_action( 'init', 'lean_carousel_category', 0 );
+
+}
