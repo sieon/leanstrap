d66525e900f186ff0bea5d52798b61d210165f39
mxy <sieon@outlook.com>
2017-04-05 22:47:03 +0800

    修改文章列表样式

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -242,114 +242,12 @@ function lean_entry_meta() {
 		printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'lean' ) );
 	}
 
-	/*
-  $format = get_post_format();
-	if ( current_theme_supports( 'post-formats', $format ) ) {
-		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'lean' ) ),
-			esc_url( get_post_format_link( $format ) ),
-			get_post_format_string( $format )
-		);
-	}
-  */
-
-	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time> ';
-
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time> <time class="updated" datetime="%3$s">%4$s</time> ';
-		}
-
-		$time_string = sprintf( $time_string,
-			esc_attr( get_the_date( 'c' ) ),
-			get_the_date()
-			//esc_attr( get_the_modified_date( 'c' ) ),
-			//get_the_modified_date()
-		);
-
-		printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span> ',
-			_x( 'Posted on', 'Used before publish date.', 'lean' ),
-			esc_url( get_permalink() ),
-			$time_string
-		);
-	}
-
-	if ( 'post' == get_post_type() ) {
-		if ( is_singular() || is_multi_author() ) {
-			printf( '<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				_x( 'Author', 'Used before post author name.', 'lean' ),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-				get_the_author()
-			);
-		}
-
-		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'lean' ) );
-		if ( $categories_list && lean_categorized_blog() ) {
-			printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span> ',
-				_x( 'Categories', 'Used before category names.', 'lean' ),
-				$categories_list
-			);
-		}
-
-		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'lean' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-				_x( 'Tags', 'Used before tag names.', 'lean' ),
-				$tags_list
-			);
-		}
-	}
-
-	if ( is_attachment() && wp_attachment_is_image() ) {
-		// Retrieve attachment metadata.
-		$metadata = wp_get_attachment_metadata();
-
-		printf( '<span class="full-size-link"><span class="screen-reader-text">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
-			_x( 'Full size', 'Used before full size attachment link.', 'lean' ),
-			esc_url( wp_get_attachment_url() ),
-			$metadata['width'],
-			$metadata['height']
-		);
-	}
-
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
 		/* translators: %s: post title */
 		comments_popup_link( sprintf( __( '去评论<span class="screen-reader-text"> on %s</span>', 'lean' ), get_the_title() ) );
 		echo '</span>';
 	}
-}
-endif;
-
-
-
-if ( ! function_exists( 'lean_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function lean_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' == get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'lean' ) );
-		if ( $categories_list && lean_categorized_blog() ) {
-			printf( '<span class="cat-links"><i class="fa fa-book fa-fw" aria-hidden="true"></i> ' . esc_html__( '%1$s', 'lean' ) . '</span>', $categories_list );
-		}
-
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'lean' ) );
-		if ( $tags_list ) {
-			printf( '&nbsp;<span class="tags-links"><i class="fa fa-tags" aria-hidden="true"></i>' . esc_html__('&nbsp;%1$s', 'lean' ) . '</span>', $tags_list );
-		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '&nbsp;<span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i>&nbsp;';
-		comments_popup_link( esc_html__( '0 条评论', 'lean' ), esc_html__( '1 条评论', 'lean' ), esc_html__( '% 条评论', 'lean' ) );
-		echo '</span> ';
-	}
-
-	edit_post_link( esc_html__( '编辑', 'lean' ), '<span class="edit-link">', '</span>' );
 }
 endif;
 
--- template-parts/content-bloglist.php
+++ template-parts/content-bloglist.php
@@ -8,10 +8,10 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('entry border-b mb-3'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('entry border-b pt-3 pb-2'); ?>>
 	<div class="row">
 		<?php if(has_post_thumbnail()) : ?>
-		<div class="col-4 col-lg-3 mb-3 pb-1">
+		<div class="col-4 col-lg-3">
 			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
 				<?php
 					// Post thumbnail.
@@ -19,23 +19,38 @@
 				?>
 			</a>
 		</div>
-		<div class="col-8 col-lg-9 mb-3 pb-1">
+		<div class="col-8 col-lg-9">
 		<?php else : ?>
-		<div class="col-12 mb-3 pt-3">
+		<div class="col-12">
 		<?php endif; ?>
 
 			<header class="entry-header mb-3">
-				<?php the_title( sprintf( '<h3 class="mb-3"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+				<?php the_title( sprintf( '<h3><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 			</header>
 
 			<div class="entry-excerpt">
 				<?php the_excerpt(); ?>
 			</div>
 
-			<footer class="entry-footer pb-2">
-				<small>
-					<?php lean_entry_meta(); ?>
-				</small>
+			<footer class="entry-footer">
+				<p>
+					<small>
+						<?php
+						if ( is_sticky() && is_home() && ! is_paged() ) {
+							printf( '<span class="sticky-post">%s</span> ', __( '特色', 'lean' ) );
+						}?>
+
+						<?php the_time();?>
+
+						<?php if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+								echo ' <span class="comments-link">';
+								/* translators: %s: post title */
+								comments_popup_link( sprintf( __( '去评论<span class="screen-reader-text"> on %s</span>', 'lean' ), get_the_title() ) );
+								echo '</span>';
+							}
+						 ?>
+					</small>
+				</p>
 			</footer><!-- .entry-footer -->
 		</div>
 
