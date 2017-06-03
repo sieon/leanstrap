9931a5247afc05585d80b0171e5289d50bd0b6f6
sieon <sieon@outlook.com>
2017-06-03 23:05:07 +0800

    修复下更新bootstrap的问题，未完成

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,11 +4,11 @@
  */
 function lean_scripts() {
 
-  wp_enqueue_style( 'lean-bootstrap', THEME_URI . '/assets/css/lean.css');
-	wp_enqueue_style( 'lean-font-awesome', '//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style( 'lean-bootstrap', THEME_URI . '/assets/css/bootstrap/bootstrap.css');
+	wp_enqueue_style( 'lean-font-awesome', THEME_URI . '/assets/css/font-awesome.min.css');
   //wp_enqueue_style( 'lean-animate', THEME_URI . '/assets/css/animate.css');
   //wp_enqueue_style( 'lean-superfish', THEME_URI . '/assets/css/superfish.css');
-  //wp_enqueue_style( 'lean-main', THEME_URI . '/assets/css/main.css');
+  wp_enqueue_style( 'lean-main', THEME_URI . '/assets/css/main.css');
 
   //jQuery first, then Tether, then Bootstrap JS.
 	wp_enqueue_script( 'lean-jquery', THEME_URI . '/assets/js/jquery.js', array(), '20170416', true );
